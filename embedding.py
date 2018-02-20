f3beb6ed84153e704b66b683f87265285bf104d4
Remy_dubois <remydubois14@gmail.com>
2018-02-20 15:57:00 +0100

    modifier the completion script for the description file, and pushed the description file completed

--- embedding.py
+++ embedding.py
@@ -14,6 +14,11 @@ import sys
 import warnings
 import argparse
 from sklearn.pipeline import Pipeline
+import io
+from PyPDF2 import PdfFileReader
+from pathlib2 import Path
+from pdfquery import PDFQuery
+import time
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -28,62 +33,122 @@ parser.add_argument('--n_clusters',
                     help="This parameter is used to choose the number of clusters in which to classify drugs.")
 
 
+def scrape_pdf(url):
+    #As the link provided in open-medicaments redirects toward the EU drug databases, the javascript needs to be
+    #scrapped as well in order to reach the right storage URL
+    r = requests.get(url, allow_redirects=True)
+    #Each drug has a number following "#" in the url
+    num_med = re.findall('#[0-9]*', url)[0].replace('#','')
+    text = r.text
+    # Mind the '080' which is refered to as '80' in the EU database
+    num_med = str(int(num_med))
+    # Extract the 'id' which is made out of a datetime and other numbers
+    try:
+        med_id = re.findall('med_nr\['+num_med+'\]\ \=\ \"[0-9]*\"',text)[0].split('"')[1]
+    except:
+        print('drug not in EU database')
+        return 'NF'
+    #Now reconstitute url of the actual storage
+    storage_url = 'http://ec.europa.eu/health/documents/community-register/'+med_id[:4]+'/'+med_id+'/anx_'+med_id[8:]+'_fr.pdf'
+    #request the actual pdf
+    r = requests.get(storage_url)
+    #Bare 
+    time.sleep(0.5)
+    f = io.BytesIO(r.content)
+    try:
+        reader = PdfFileReader(f)
+    except:
+        #Get thrown when the pdf is not compiled the right way
+        print('Error while parsing pdf at url: %s'%(url))
+        return 'NF'
+    #get text which might be spread on page 2 and 3
+    contents = reader.getPage(1).extractText() + reader.getPage(2).extractText()
+    try:
+        indication = re.findall('(?<=Indications thérapeutiques)(.*)(?=4.2)', contents.replace('\n',''))[0]
+    except IndexError:
+        # Some pdf are not readable: they do not seem to contain any text (at least, pypdf does not find any text)
+        print('Not readable PDF')
+        return 'NF'
+    return indication
+
+
 # Requesting open-medicaments API to get therapeutic indications for each drug
 def grab_indications(ide):
     a = requests.get('https://open-medicaments.fr/api/v1/medicaments/%s' % (ide))
     json = a.json()
     try:
         html = json['indicationsTherapeutiques']
-        soup = BeautifulSoup(html, 'html.parser')
-        indication = str(soup.get_text())
+        # If the json contains an hyperlink, then go to this hyperlink, otherwise parse the content of 'indicationsTherapeutiques'
+        if ' Vous trouverez les indications thérapeutiques de ce médicament dans le paragraphe 4.1 du RCP ou dans le paragraphe 1 de la notice.' in html:
+            # Get the hyperlink out of the json
+            link = re.search('http:\/\/[a-z.\/\-\#0-9_]*',html)[0]
+            indication = scrape_pdf(link)
+        else:
+            #Parse the html
+            soup = BeautifulSoup(html, 'html.parser')
+            indication = str(soup.get_text())
     except KeyError:
         indication = 'NF'
 
+    #get rid of some trash    
     indication = re.sub(FORBIDDEN_INDICATIONS[0], 'NF', indication)
     indication = re.sub("Plus d'information en cliquant ici", '', indication)
-    indication = re.sub(
-        ' Vous trouverez les indications thérapeutiques de ce médicament dans le paragraphe 4.1 du RCP ou dans le paragraphe 1 de la notice. Ces documents sont disponibles en cliquant ici ',
-        'NF', indication)
 
     return indication
 
 def complete(df):
+    #Check for existing description file:
+    description_file = './results/indications'
+    #If an existing description file is found, then open it in order to complete it, otherwise create a new empty dataframe.
+    if Path(description_file).is_file():
+        print('Description file found.')
+        existing_indications = pandas.read_csv(description_file, index_col=0)
+    else:
+        print('No description file found… Creating description file.')
+        existing_indications = pandas.DataFrame(columns=['drug_names','descriptions'])
+
+    #Get the drugs present in the train dataset
     print('Completing database…')
     drugs_in_train = df['drug_names'].apply(lambda s: unidecode(str(s)))
     drugs_in_train = [n for s in drugs_in_train for n in s.split(',')]
     drugs_df = pandas.read_csv('/Users/remydubois/Documents/perso/repos/rd_repos/posos-challenge/results/drug_names')
 
+    #get all the ids related to a drug name, in order to query against open-medicaments with all the ids. If a short name is related to severla
+    # ids, then all the description should same (or at least very similar), therefore, I just take the first description that pops out.
     dico = {}
     for n in np.unique(drugs_in_train):
         dico[n] = list(drugs_df[drugs_df.drug_names == n].drug_ids)
 
-    remaining = dico.keys()  # list(filter(lambda s: s=='NF', dico.keys()))
+    #Get the missing descriptions
+    remaining = [k for k in dico.keys() if k not in list(existing_indications.drug_names)]
     
+    #Go through all the short names
     for n in tqdm.tqdm(remaining):
         indication = 'NF'
         i = 0
+        #For a short name, ho through all its ids
         while indication == 'NF' and i < len(dico[n]):
             try:
                 indication = grab_indications(dico[n][i])
-                # print(indication)
                 i += 1
+
+            #If interrupted, save what has been retrieved so far.
             except KeyboardInterrupt:
                 print('Interrupted, saving retrieved information.')
-                frame = pandas.DataFrame(list(dico.items()), columns=['drug_names', 'descriptions'])
-                frame.to_csv('/Users/remydubois/Documents/perso/repos/rd_repos/posos-challenge/results/indications',
+                existing_indications.to_csv('/Users/remydubois/Documents/perso/repos/rd_repos/posos-challenge/results/indications',
                              index=True)
                 try:
                     sys.exit(0)
                 except SystemExit:
                     os._exit(0)
 
-        dico[n] = indication
+        existing_indications = existing_indications.append(pandas.DataFrame([[n,indication]], columns=['drug_names','descriptions']))
 
-    frame = pandas.DataFrame(list(dico.items()), columns=['drug_names', 'descriptions'])
-    frame.to_csv('/Users/remydubois/Documents/perso/repos/rd_repos/posos-challenge/results/indications', index=True)
+    #frame = pandas.DataFrame(list(dico.items()), columns=['drug_names', 'descriptions'])
+    existing_indications.to_csv('/Users/remydubois/Documents/perso/repos/rd_repos/posos-challenge/results/indications', index=True)
     print('DB completed')
 
-    return frame
+    return existing_indications
 
 # As of now: this object does not handle the 'NF', however they are rejected by the tfidf thanks to 
 # the parameter max_df. It means that drugs with no descriptions ('NF' for 'Not Found') have zero-coordinates
